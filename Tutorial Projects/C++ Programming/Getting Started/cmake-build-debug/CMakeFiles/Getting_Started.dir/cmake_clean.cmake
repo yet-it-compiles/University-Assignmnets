732598206f371457160b5b086b15b6c523076c5e
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-01-01 19:33:43 -0800

    Initial Submissions

---
+++ Tutorial Projects/C++ Programming/Getting Started/cmake-build-debug/CMakeFiles/Getting_Started.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Getting_Started.dir/main.cpp.obj"
+  "Getting_Started.exe"
+  "Getting_Started.exe.manifest"
+  "Getting_Started.pdb"
+  "libGetting_Started.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Getting_Started.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tutorial Projects/C++ Programming/Getting Started/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl; // simple output to console
+    cout << "Tyler said \"I will get the hang of C++\"" << endl; // using escape sequences
+
+
+}
