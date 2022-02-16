206eb82a3a0445e71d8cea0c06883527378f8204
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-02-16 11:30:00 -0800

    Delete Tutorial Projects directory

--- Tutorial Projects/Java - Data Structures/InterfaceTutorial/src/Tutorial/Main.java
+++
@@ -1,8 +0,0 @@
-package Tutorial;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
--- Tutorial Projects/Python Programming/Output Formatting - Math.py
+++
@@ -1,8 +0,0 @@
-""" Simple module file covers output formatting """
-import math
-
-print("Using division = ", 15 / 4)  # should be 3
-print("Using Modulus = ", 14 % 4)  # should be 2, as two is the remainder of the above operation
-
-print("Fmod = ", math.fmod(15, 4))
-
