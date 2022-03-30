97765f9b4ce07f5ff1dc20b9b8dccf80ad1ba2d3
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-03-30 10:30:51 -0700

    Lab #1 - Visual Basic - Hello World
    This is a simple module which displays hello world in VB

---
+++ CS 105 - Logical Basis of Computing/Lab 1/Unsworth-Lab1/HelloWorld/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnDisplayMessage = New System.Windows.Forms.Button()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnDisplayMessage
+        '
+        Me.btnDisplayMessage.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnDisplayMessage.Location = New System.Drawing.Point(414, 183)
+        Me.btnDisplayMessage.Name = "btnDisplayMessage"
+        Me.btnDisplayMessage.Size = New System.Drawing.Size(128, 46)
+        Me.btnDisplayMessage.TabIndex = 0
+        Me.btnDisplayMessage.Text = "Display Message"
+        Me.btnDisplayMessage.UseVisualStyleBackColor = True
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblMessage.Location = New System.Drawing.Point(430, 153)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(96, 15)
+        Me.lblMessage.TabIndex = 1
+        Me.lblMessage.Text = "Click the button"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1450, 1174)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.btnDisplayMessage)
+        Me.Name = "Form1"
+        Me.Text = "HelloWorld"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnDisplayMessage As Button
+    Friend WithEvents lblMessage As Label
+End Class
---
+++ CS 105 - Logical Basis of Computing/Lab 1/Unsworth-Lab1/HelloWorld/Form1.vb
@@ -0,0 +1,9 @@
+﻿' Tyler S. Unsworth
+' I pledge that I have neither given nor received help from anyone 
+' other than the instructor and TA for all program components included here
+Public Class Form1
+    Private Sub btnDisplayMessage_Click(sender As Object, e As EventArgs) Handles btnDisplayMessage.Click
+        'change the Text property of lblMessage
+        lblMessage.Text = "Hello World!"
+    End Sub
+End Class
