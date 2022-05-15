e2e4ab6837f08fa42e68d10bc10b16b0045959de
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-05-15 08:42:21 -0700

    Add files via upload

--- Refining-JavaScript-Skills-ES6/chapters-1-and-2.js
+++ Refining-JavaScript-Skills-ES6/chapters-1-and-2.js
@@ -1,1 +1,30 @@
-console.log("hello");
+/**
+ * Simple module which practices the concepts of the first two chapters of the definitive JS guide.
+ */
+
+// Declares variables
+let firstIntro = "Hello";
+const langauge = " JavaScript"
+let secondIntro = ", my new friend!";
+
+let minimumAge = 0;
+
+
+/**
+ * Simple function which prints the standard "Hello new language" phrase
+ */
+function printIntro(){
+    let intro = firstIntro + `${langauge}` + secondIntro
+    console.log(intro)
+    console.log("\u{1F600}"); // prints a smiling face
+}
+
+function incrementNumber(){
+    while (minimumAge < 18){
+        minimumAge++;
+    }
+    console.log("You should now be 18, since 18=" + minimumAge)
+}
+
+printIntro(firstIntro, secondIntro)
+incrementNumber()
