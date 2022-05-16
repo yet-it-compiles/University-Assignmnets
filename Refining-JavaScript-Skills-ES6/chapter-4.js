953400cf99254a6e2b350e1de443adc07a53d269
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-05-16 11:15:05 -0700

    Nearly finished with Chapter 4 notes

--- Refining-JavaScript-Skills-ES6/chapter-4.js
+++ Refining-JavaScript-Skills-ES6/chapter-4.js
@@ -18,19 +18,22 @@ function propertyAccess() {
 function increment() {
     let initialValue = 10;
 
-    console.log("Incremented Value by 1 after printed: " + initialValue++); // goes to 11
+    console.log("\nIncremented Value by 1 after printed: " + initialValue++); // goes to 11
     console.log("New value after increment: " + initialValue); // prints 11
 
     console.log("Value after decrement: " + initialValue--) // prints 11 -> goes to 10
     console.log("Negates the value: " + -initialValue); // Negates the value
 }
 
+/**
+ * Simple method which explores the differences of equality
+ */
 function equality() {
     let string = "123456";
     let number = 123456;
 
     if (string == number) // passes: Not strict operator [like below] && JS auto converts to the necessary type
-        console.log("This will pass");
+        console.log("\nThis will pass");
 
     if (string === number) // fails: This happens because we are using a strict operator, where there is no auto cast
         console.log("This will surely not pass");
@@ -42,10 +45,29 @@ function equality() {
         console.log("This ")
 }
 
+/**
+ * Explores the 'in' operator
+ *
+ * Checks to see if a property [leftValue] is contained in the object[rightValue]
+ */
+function inOperator() {
+    let object = {x: 1, y: 3};
+    let array = [1,2,3];
+
+    console.log("\nx" in object); // evaluates to true since 'x' is a property label
+    console.log(0 in array); // evaluates to true since 0 is the property label
+
+    let d = new Date();
+    console.log("\nTrue, as d was created with Date(): ", d instanceof Date);
+    console.log("True, as all objects are instanceof Object ", d instanceof Object);
+    console.log("False, as d is not a number, but an Object && Date(): ", d instanceof Number);
+}
+
 
 propertyAccess()
 increment()
 equality()
+inOperator()
 
 //           ========================= CHAPTER 4: Notes  =========================
 
