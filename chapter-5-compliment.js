74bf8df3393a722c75536946f62466e703aa63ac
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-05-17 08:59:58 -0700

    Delete chapter-5-compliment.js

--- chapter-5-compliment.js
+++
@@ -1,8 +0,0 @@
-/**
- * A simple module which practices the concepts of each section in Chapter 5 of David Flanagan's:
- * JavaScript - The Definitive Guide
- */
-
-import { name } from './chapter-5.js';
-console.log("\nThis module")
-console.log(`The title from the other module was `, name);
