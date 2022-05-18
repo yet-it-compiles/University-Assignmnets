c44c6f978af672985917dddb1067662857a6828c
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-05-17 17:15:13 -0700

    Progressing through chapter 6 exercises

---
+++ Refining-JavaScript-Skills-ES6/chapter-6.js
@@ -0,0 +1,69 @@
+/**
+ * A simple module which practices the concepts of each section in Chapter 6 of David Flanagan's:
+ * JavaScript - The Definitive Guide
+ */
+
+/*                                              Key Chapter Takeaways
+1. - Any value in JavaScript that is not a string, a number, a Symbol, or true, false, null, or undefined is an object.
+2. - Object.prototype is one of the rare objects that has no prototype in that it does not inherit any properties.
+3. - You can pass null to create a new object that does not have a prototype, but if you do this, the newly created
+object will not inherit anything, not even basic methods like toString()
+4. - One use for Object.create() is when you want to guard against unintended (but non-malicious) modification of an
+object by a library function that you don’t have control over.
+ */
+
+
+//           ========================= Creating Object with Object.create()  =========================
+// TODO: One use for Object.create() is when you want to guard against unintended (but non-malicious) modification
+
+
+/**
+ * Object.create() creates a new object, using its first argument as the prototype of that object:
+ */
+function objectWithParams() {
+    let object = Object.create({x: 1, y: 2});
+    console.log("\nObject with parameters: ", object) // prints nothing
+}
+
+/**
+ * Showcases the output of an object when null is passed
+ */
+function objectWithNull() {
+    let object = Object.create(null); // goes with Chapter Takeaway 3.
+
+    console.log("\nObject with null: ", object);
+}
+
+/**
+ *
+ */
+function objectEmpty() {
+    let object = Object.create(Object.prototype);
+    console.log("\nObject with Object.prototype: ", object)
+}
+
+objectWithParams();
+objectWithNull();
+objectEmpty();
+objectPractice();
+
+function objectPractice() {
+    let object = {};
+
+    object.x = "bmx";
+    object.y = "yet-it-compiles";
+
+    let newObject = Object.create(object);
+
+    console.log("\nObject prints by dot operator: ", object.x)
+    delete object.x;
+    console.log("What happens when a property is deleted: ", object["x"]);
+
+    if (delete object.y){
+        console.log("This is how you can use it as a boolean operator.")
+    }
+}
+
+function printObjectProperties() {
+
+}
