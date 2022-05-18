bccc94eefed9c3c7670f527f517210ba1daa2036
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2022-05-18 10:06:05 -0700

    Updated page to show a dynamic list

--- My Road With React/src/App.js
+++ My Road With React/src/App.js
@@ -1,13 +1,47 @@
 import React from 'react';
 
-const welcome = {greeting: "Hey", title: "React", }
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
 
 function App() {
 
-  return (
-      <div>
-        <h1>{welcome.greeting} {welcome.title}</h1>
-      </div>
-  );
+    return (
+        <div>
+            <h1>My Hacker Stories</h1>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text"/>
+            <hr/>
+            {list.map(function (item) {
+                return (
+                    <div key={item.objectID}>
+                        <span>
+                        <a href={item.url}>{item.title}</a>
+                        </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                    </div>
+                );
+            })}
+
+        </div>
+    );
 }
+
 export default App;
